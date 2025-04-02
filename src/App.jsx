e30a08886b7a8fc84e9c35d444aab56dbdca9247
hanznikkol <hanznikkolmaas@gmail.com>
2025-04-02 21:49:07 +0800

    Tommorow again 04/02/25

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import signUpMobile from './assets/img/illustration-sign-up-mobile.svg'
 function App() {
 
   return (
     <>
+      <div className="w-full min-h-screen bg-neutral-blue-2 flex flex-col">
+          <div className="w-full flex-1 flex flex-col bg-white">
+            <div className="w-full h-[30vh] flex flex-col bg-red-200">
+              
+            </div>
+          </div>
+      </div>
     </>
   )
 }
